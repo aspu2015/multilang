10af6b6ffd9d6d800f48e1975849d5d185725542
Nikivas <nikivas97@mail.ru>
2019-06-28 10:29:02 +0400

    migrations

---
+++ database/migrations/2019_06_28_054727_create_translations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTranslationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('translations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('university_id');
+            //$table->foreign('university_id')->references('id')->on('universities');
+            $table->integer('language_id');
+            //$table->foreign('language_id')->references('id')->on('languages');
+            $table->string('name');
+            $table->string('text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('translations');
+    }
+}
