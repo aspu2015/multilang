4ddd398974821c68d3aecf9b4cfca8c5c290ffd7
Nikivas <nikivas97@mail.ru>
2019-07-01 17:10:42 +0400

    view text changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -90,7 +90,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Universities
                 </div>
 
                 
